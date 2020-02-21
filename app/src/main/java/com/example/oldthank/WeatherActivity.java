25c9b9cb24c5770b93a23ddbcf07af71f0d72b9c
OldThank <404290996@qq.com>
2020-02-21 16:28:27 +0800

    添加后台自动更新天气功能

--- app/src/main/java/com/example/oldthank/WeatherActivity.java
+++ app/src/main/java/com/example/oldthank/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.oldthank.gson.Forecast;
 import com.example.oldthank.gson.Weather;
+import com.example.oldthank.service.AutoUpdateService;
 import com.example.oldthank.util.HttpUtil;
 import com.example.oldthank.util.LogUtil;
 import com.example.oldthank.util.Utility;
@@ -234,5 +236,9 @@ public class WeatherActivity extends AppCompatActivity {
          * ScrollView 重新可见
          * */
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 }
